01d5f0f0d69de636245ed48bcd4128f5896a1462
TrentonBowser <trenton.bowser25@gmail.com>
2018-01-22 20:13:30 -0600

    Added velocity output

--- MotionMagicPID/src/org/usfirst/frc/team930/robot/Robot.java
+++ MotionMagicPID/src/org/usfirst/frc/team930/robot/Robot.java
@@ -37,13 +37,13 @@ public class Robot extends TimedRobot {
 		test1 = SmartDashboard.putNumber("I Value", 0.0);
 		test1 = SmartDashboard.putNumber("D Value", 0.0);
 		test1 = SmartDashboard.putNumber("F Value", 0.2);
-		test1 = SmartDashboard.putNumber("Cruise Velocity", 15000);
-		test1 = SmartDashboard.putNumber("Acceleration", 6000);
+		test1 = SmartDashboard.putNumber("Cruise Velocity", 5000);
+		test1 = SmartDashboard.putNumber("Acceleration", 2000);
 		test1 = SmartDashboard.putBoolean("Update Values", false);
 		test1 = SmartDashboard.putNumber("Position", motor1.getSelectedSensorPosition(0));
 		test1 = SmartDashboard.putNumber("Motor Output", motor1.getMotorOutputPercent());
 		test1 = SmartDashboard.putNumber("Closed Loop Error",0);
-		test1 = SmartDashboard.putNumber("Taget Position", 0);
+		test1 = SmartDashboard.putNumber("Velocity", motor1.getSelectedSensorVelocity(0));
 		
 		motor1.selectProfileSlot(0, 0);
 		// Initialize PIDF values, velocity, and acceleration
@@ -51,8 +51,8 @@ public class Robot extends TimedRobot {
 		motor1.config_kP(0, 0.2, 10);
 		motor1.config_kI(0, 0, 10);
 		motor1.config_kD(0, 0, 10);
-		motor1.configMotionCruiseVelocity(15000, 10);	//(int sensorUnitsPer100ms, int timeoutMs)
-		motor1.configMotionAcceleration(6000, 10);		//(int sensorUnitsPer100msPerSec, int timeoutMs)
+		motor1.configMotionCruiseVelocity(5000, 10);	//(int sensorUnitsPer100ms, int timeoutMs)
+		motor1.configMotionAcceleration(2000, 10);		//(int sensorUnitsPer100msPerSec, int timeoutMs)
 		motor1.setSelectedSensorPosition(0, 0, 10);
 	}
 
@@ -77,14 +77,15 @@ public class Robot extends TimedRobot {
 			motor1.config_kP(0, SmartDashboard.getNumber("P Value", 0.2), 10);
 			motor1.config_kI(0, SmartDashboard.getNumber("I Value", 0.0), 10);
 			motor1.config_kD(0, SmartDashboard.getNumber("D Value", 0.0), 10);
-			motor1.configMotionCruiseVelocity((int) SmartDashboard.getNumber("Cruise Velocity", 15000), 10);
-			motor1.configMotionAcceleration((int)SmartDashboard.getNumber("Acceleration", 6000), 10);
+			motor1.configMotionCruiseVelocity((int) SmartDashboard.getNumber("Cruise Velocity", 5000), 10);
+			motor1.configMotionAcceleration((int)SmartDashboard.getNumber("Acceleration", 2000), 10);
 			System.out.println("Updated Values");
 			test1 = SmartDashboard.putBoolean("Update Values", false);
 		}
 		
 		test1 = SmartDashboard.putNumber("Position", motor1.getSelectedSensorPosition(0));
 		test1 = SmartDashboard.putNumber("Motor Output", motor1.getMotorOutputPercent());
+		test1 = SmartDashboard.putNumber("Velocity", motor1.getSelectedSensorVelocity(0));
 		
 		if(controller.getRawButton(1) && (!aPressed))
 		{
@@ -100,7 +101,6 @@ public class Robot extends TimedRobot {
 			double targetPos = controller.getRawAxis(1) * 4096 * -10.0; /* 4096 ticks/rev * 10 Rotations in either direction */
 			motor1.set(ControlMode.MotionMagic, targetPos);
 			test1 = SmartDashboard.putNumber("Closed Loop Error",motor1.getClosedLoopError(0));
-			test1 = SmartDashboard.putNumber("Taget Position", targetPos);
 		} else {
 			if (controller.getRawAxis(1) > 0.2 || controller.getRawAxis(1) < -0.2) {
 				motor1.set(ControlMode.PercentOutput, controller.getRawAxis(1));
