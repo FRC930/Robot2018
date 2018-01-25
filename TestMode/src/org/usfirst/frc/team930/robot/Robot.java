5530a5fda8f910cea1ec7e200311f829d0ad0766
TrentonBowser <trenton.bowser25@gmail.com>
2018-01-24 19:56:17 -0600

    added arrays to shuffleboard

--- TestMode/src/org/usfirst/frc/team930/robot/Robot.java
+++ TestMode/src/org/usfirst/frc/team930/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team930.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -13,16 +6,32 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+	
 	TalonSRX motor1 = new TalonSRX(0);
+	boolean empty;
+	boolean buttons[] = new boolean[10];
+	String functionNames[] = new String[10];
+	String functionOutcome[] = new String[10];
 
 	@Override
 	public void robotInit() {
-		SmartDashboard.putNumber("Talon Output", motor1.getMotorOutputPercent());
-		SmartDashboard.putBoolean("Update Values", false);
+		
+		for (int x = 0; x < 10; x++) {
+			buttons[x] = false;
+		}
+		functionNames[0] = "Left Drive Encoder";
+		functionNames[1] = "Right Drive Encoder";
+		functionNames[2] = "Elevator Encoder";
+		
+		empty = SmartDashboard.putNumber("Talon Output", motor1.getMotorOutputPercent());
+		empty = SmartDashboard.putBoolean("Update Values", false);
+		empty = SmartDashboard.putBooleanArray("Buttons", buttons);
+		empty = SmartDashboard.putStringArray("Functions", functionNames);
+		empty = SmartDashboard.putStringArray("Function Outcome", functionOutcome);
+		
 		
 		LiveWindow.add((Sendable)motor1);
 		((Sendable) motor1).setName("Subsystem", "Talon");
@@ -38,27 +47,15 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		//update values using SmartDashboard
-		if(SmartDashboard.getBoolean("Update Values", false))
-		{
-			motor1.set(ControlMode.PercentOutput, SmartDashboard.getNumber("Talon Output", 0));
-			SmartDashboard.putBoolean("Update Values", false);
-			
-			System.out.println("Updated Values");
-		}
-		SmartDashboard.putNumber("Talon Output", motor1.getMotorOutputPercent());
 	}
-
-	/**
-	 * This function is called periodically during test mode.
-	 */
+	
 	@Override
 	public void testPeriodic() {
 		//update values using SmartDashboard
 		if(SmartDashboard.getBoolean("Update Values", false))
 		{
 			motor1.set(ControlMode.PercentOutput, SmartDashboard.getNumber("Talon Output", 0));
-			SmartDashboard.putBoolean("Update Values", false);
+			empty = SmartDashboard.putBoolean("Update Values", false);
 			
 			System.out.println("Updated Values");
 		}
