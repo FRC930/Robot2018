52ce05094e17c245d5cdddd9ed947c38667caec8
SamMurphy1 <samanthamurphy@wi.rr.com>
2018-01-25 20:40:30 -0600

    AutoChooser and main robot code

---
+++ 2018RobotCode/src/org/usfirst/frc/team930/robot/Robot.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team930.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends IterativeRobot {
+
+	@Override
+	public void robotInit() {
+		
+		//Drive.init();
+		//Intake.init();
+		//Elevator.init();
+		//autoChooser + SmartDashboard
+		
+	}
+
+	@Override
+	public void autonomousInit() {
+		
+		//Getting auto info
+		//Auto notifier
+		
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		
+		//Drive.run();
+		//Intake.run();
+		//Elevator.run();
+		
+	}
+
+	@Override
+	public void testPeriodic() {
+		
+	}
+}
