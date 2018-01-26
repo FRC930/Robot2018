080662b79c8771b05f33982234bac35fcb33641f
MasterMemeMinister <soupcateatternom@gmail.com>
2018-01-25 19:42:55 -0600

    Added an LEDCode project for testing LEDs with Arduino

---
+++ LEDTest/src/org/usfirst/frc/team930/robot/Robot.java
@@ -0,0 +1,79 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team930.robot;
+
+import javax.xml.bind.annotation.XmlElementDecl.GLOBAL;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot extends TimedRobot {
+	
+	static I2C wire = new I2C(Port.kOnboard, 4);
+	Joystick controller = new Joystick(0);
+
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+
+	
+	@Override
+	public void robotInit() {
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+	}
+
+	
+	@Override
+	public void autonomousInit() {
+		m_autoSelected = m_chooser.getSelected();
+		// m_autoSelected = SmartDashboard.getString("Auto Selector",
+		// 		kDefaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
+	}
+
+	
+	@Override
+	public void autonomousPeriodic() {
+		switch (m_autoSelected) {
+			case kCustomAuto:
+				// Put custom auto code here
+				break;
+			case kDefaultAuto:
+			default:
+				// Put default auto code here
+				break;
+		}
+	}
+
+	
+	@Override
+	public void teleopPeriodic() {
+		if (controller.getRawButton(2)) {
+			String WriteString = "go";
+			char[] CharArray = WriteString.toCharArray();
+			byte[] WriteData = new byte[CharArray.length];
+			for (int i = 0; i < CharArray.length; i++) {
+				WriteData[i] = (byte) CharArray[i];
+			}
+			wire.transaction(WriteData, WriteData.length, null, 0);
+		}
+	}
+
+	
+	@Override
+	public void testPeriodic() {
+	}
+}
