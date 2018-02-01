e9e90c2a9c9fa1bebce7182a043e2f18bcf6c0a2
devin132 <milligandm@hotmail.com>
2018-01-31 20:33:09 -0600

    Fixed Gyro correction on motion profila

--- TestaForLibriOnMotionProfila/src/org/usfirst/frc/team930/robot/Robot.java
+++ TestaForLibriOnMotionProfila/src/org/usfirst/frc/team930/robot/Robot.java
@@ -31,9 +31,45 @@ public class Robot extends IterativeRobot {
 	Timer time = new Timer();
 	
 	Waypoint[] points = new Waypoint[] {
-		    new Waypoint(0, 0, Pathfinder.d2r(0)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
-		    //new Waypoint(2, 0, Pathfinder.d2r(0)),  
-		    new Waypoint(5,3, Pathfinder.d2r(90)),
+//		    new Waypoint(0, 0, Pathfinder.d2r(0)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
+//		    new Waypoint(3,0,0),
+//		    new Waypoint(5,-2,Pathfinder.d2r(270)),
+//		    new Waypoint(7,-4,Pathfinder.d2r(0)),
+//		    new Waypoint(9,-2, Pathfinder.d2r(90)),
+//		    new Waypoint(7,0,Pathfinder.d2r(180)),
+//		    new Waypoint(5,-2,Pathfinder.d2r(270)),
+//		    new Waypoint(3,-4,Pathfinder.d2r(180)),
+//		    new Waypoint(1,-2, Pathfinder.d2r(90)),
+//		    new Waypoint(3,0,0)
+			new Waypoint(0, 0, Pathfinder.d2r(0)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
+		    new Waypoint(4,0,0),
+		    new Waypoint(6,-2,Pathfinder.d2r(270)),
+		    new Waypoint(4,-4,Pathfinder.d2r(180)),
+		    new Waypoint(2,-2, Pathfinder.d2r(90)),
+		    new Waypoint(4,0,0),
+		    new Waypoint(6,-2,Pathfinder.d2r(270)),
+		    new Waypoint(4,-4,Pathfinder.d2r(180)),
+		    new Waypoint(2,-2, Pathfinder.d2r(90)),
+		    new Waypoint(4,0,0),
+		    new Waypoint(6,-2,Pathfinder.d2r(270)),
+		    new Waypoint(4,-4,Pathfinder.d2r(180)),
+		    new Waypoint(2,-2, Pathfinder.d2r(90)),
+		    new Waypoint(4,0,0),
+		    new Waypoint(6,-2,Pathfinder.d2r(270)),
+		    new Waypoint(4,-4,Pathfinder.d2r(180)),
+		    new Waypoint(2,-2, Pathfinder.d2r(90)),
+		    new Waypoint(4,0,0),
+			 /*new Waypoint(0, 0, Pathfinder.d2r(0)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
+			    new Waypoint(3,0,0),
+			    new Waypoint(4,-1,Pathfinder.d2r(315)),
+			    new Waypoint(5,-2,Pathfinder.d2r(0)),
+			    new Waypoint(6,-1, Pathfinder.d2r(90)),
+			    new Waypoint(5,0,Pathfinder.d2r(180)),
+			    new Waypoint(4,-1,Pathfinder.d2r(225)),
+			    new Waypoint(3,-2,Pathfinder.d2r(180)),
+			    new Waypoint(2,-1, Pathfinder.d2r(90)),
+			    new Waypoint(3,0,0)*/
+		    
 		   // new Waypoint(2,6,Pathfinder.d2r(180)),
 		   // new Waypoint(5.5,5,Pathfinder.d2r(180))// Waypoint @ x=-2, y=-2, exit angle=0 radians
 		};
@@ -72,9 +108,10 @@ public class Robot extends IterativeRobot {
 		leftFollow.follow(leftMain);   //respective talons
 		rightFollow2.follow(rightMain);   //Sets the victors to follow their 
 		leftFollow2.follow(leftMain); */
+		gyro.reset();
 		
-		enc.configurePIDVA(0.9, 0, 0, 0.3, 0.08);
-		enc2.configurePIDVA(0.9, 0, 0, 0.3, 0.08);
+		enc.configurePIDVA(0.9, 0, 0, 0.2, 0.08); //Ka = 0.3
+		enc2.configurePIDVA(0.9, 0, 0, 0.2, 0.08);
 		rightFollow2.follow(rightMain);
 		rightFollow.follow(rightMain);
 		leftFollow2.follow(leftMain);
@@ -101,12 +138,22 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void autonomousPeriodic() {
 		if(time.get()>1){
+		
 		double heading = Math.toDegrees(enc.getHeading());
-		double yaw = gyro.getYaw()+180;//(Math.toDegrees(enc.getHeading())-180)%180;
-		double angle_difference = heading - yaw;
+		if(heading >180)
+			heading = heading%180-180;
+		double yaw = gyro.getYaw();//(Math.toDegrees(enc.getHeading())-180)%180;
+		/*if(gyro.getYaw()<0)
+			yaw = yaw + 360;*/
+		
+		double error = heading + yaw;
+		if(error>180)
+			error = error-360;
+		else if(error < -180)
+			error = error+360;
 		double kG = 0.8 * (-1.0/80.0);
 
-		double turn = kG * angle_difference;
+		double turn = kG * error;
 			
 		/*System.out.println("GYRO YAW value:   " +  gyro.getYaw());
 		System.out.println("HEading we are shooting for:  " + Math.toDegrees(enc.getHeading()));
@@ -141,7 +188,9 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		
 		/*gyro.setAngleAdjustment(180);*/
-		System.out.println(gyro.getYaw()+180);
+		System.out.println(gyro.getAngle()%360);
+		
+		
 		
 		double xStick = -stick.getRawAxis(4);
 		double yStick = stick.getRawAxis(1);
