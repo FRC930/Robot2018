0a7111d4fe1c804b3e90a433540fed428292d45b
devin132 <milligandm@hotmail.com>
2018-02-12 18:20:21 -0600

    updateDashoboard added

--- 2018RobotCode/src/org/usfirst/frc/team930/robot/Drive.java
+++ 2018RobotCode/src/org/usfirst/frc/team930/robot/Drive.java
@@ -26,6 +26,7 @@ public class Drive {
 	}
 	
 	public static void run(double xStick, double yStick){
+		updateDashboard();
 		if(Math.abs(xStick) < Constants.deadBand)
 			xStick = 0;
 		if(Math.abs(yStick) < Constants.deadBand)
--- 2018RobotCode/src/org/usfirst/frc/team930/robot/Elevator.java
+++ 2018RobotCode/src/org/usfirst/frc/team930/robot/Elevator.java
@@ -7,6 +7,8 @@ import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Elevator {
 	// Object Declarations
 	public static TalonSRX lift1 = new TalonSRX(Constants.liftTalonID);
@@ -62,6 +64,7 @@ public class Elevator {
 
 	// Set motor's target position based on enum passed through
 	public static void run(Enum pos1) {
+		updateDashboard();
 		stateEnum = (ElevatorStates) pos1;
 		
 		switch(stateEnum) {
@@ -118,5 +121,8 @@ public class Elevator {
 	// Returns the actual position of the elevator
 	public double getPosition() {
 		return lift1.getSelectedSensorPosition(0);
+	}
+	public static void updateDashboard(){
+		SmartDashboard.putNumber("Elevator Encoder",lift1.getSelectedSensorPosition(0));
 	}
 }
