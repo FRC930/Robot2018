115e95116e5074199b019320a03d82acd7f08e59
SamMurphy1 <samanthamurphy@wi.rr.com>
2018-02-13 17:03:58 -0600

    Added Utilities methods

--- 2018RobotCode/src/org/usfirst/frc/team930/robot/Utilities.java
+++ 2018RobotCode/src/org/usfirst/frc/team930/robot/Utilities.java
@@ -7,5 +7,17 @@ public class Utilities {
 	
 	public static Compressor comp = new Compressor(0);
 	public static PowerDistributionPanel pdp = new PowerDistributionPanel(0);
+	
+	public static void turnOnCompressor() {
+		
+		comp.setClosedLoopControl(true);
+		
+	}
+	
+	public static void getPDPCurrent() {
+		
+		pdp.getCurrent(Constants.pdpIntakePort);
+		
+	}
 
 }
