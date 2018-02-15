8dd4e685eca590635130eabdca6ff0f61a46a741
MasterMemeMinister <soupcateatternom@gmail.com>
2018-02-14 20:34:14 -0600

    Updted LEDTest, updated ino code, broke LED strip

--- LEDTest/src/org/usfirst/frc/team930/robot/LEDCode/LEDCode.ino/LEDCode.ino.ino
+++ LEDTest/src/org/usfirst/frc/team930/robot/LEDCode/LEDCode.ino/LEDCode.ino.ino
@@ -9,26 +9,56 @@
 
 // This example code is in the public domain.
 
-
 #include <Wire.h>
+#include <Adafruit_DotStar.h>
+#include <SPI.h>
 
+#define NUMPIXELS 120
+#define CLOCKPIN 11
+#define DATAPIN 10                                                    
+Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+  
 void setup() {
-  Wire.begin(90);                // join i2c bus with address #90
+  Wire.begin(84);                // join i2c bus with address #84
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);           // start serial for output
+
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+    clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+  #endif
+
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
 }
 
 void loop() {
-  delay(100);
+
+  strip.setBrightness(50);
+  
+  for (int i = 0; i < NUMPIXELS; i++) { 
+    strip.setPixelColor(i, 0, 0, 200);
+    strip.show();
+    delay(50);
+    Serial.println(i);
+  }
+  strip.show();
+  delay(50);
+  for (int i = 0; i < NUMPIXELS; i++) { 
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+  strip.show();
+  
+  delay(50);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
-  while (1 < Wire.available()) { // loop through all but the last
+  while (Wire.available()) { // loop through all but the last
     char c = Wire.read(); // receive byte as a character
     Serial.print(c);         // print the character
+
+    int x = Wire.read();    // receive byte as an integer
+    Serial.println(x);         // print the integer
   }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
 }
