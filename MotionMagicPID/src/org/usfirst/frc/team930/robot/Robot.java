a5525c7e7fa176a6ca4f68db27e01a15f7484b11
TrentonBowser <trenton.bowser25@gmail.com>
2018-02-15 18:33:29 -0600

    updated controls

--- MotionMagicPID/src/org/usfirst/frc/team930/robot/Robot.java
+++ MotionMagicPID/src/org/usfirst/frc/team930/robot/Robot.java
@@ -6,6 +6,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 
+import java.util.concurrent.TimeUnit;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
@@ -15,7 +17,6 @@ public class Robot extends TimedRobot {
 	TalonSRX _talon = new TalonSRX(6);
 	Joystick _joy = new Joystick(0);
 	StringBuilder _sb = new StringBuilder();
-	boolean stopBool = false;
 	double kF = 1.0, kP = 1.0, kI = 0.002, kD = 10.0, targetPos = 6500, returnPos = 0, softLimF = 6500, softLimR = 0;
 	int velocity = 800, accel = 800;
 	
@@ -97,7 +98,7 @@ public class Robot extends TimedRobot {
 			System.out.println("Values Updated");
 			SmartDashboard.putBoolean("Update Values", false);
 		} 
-		/*
+
 		// calculate the percent motor output
 		double motorOutput = _talon.getMotorOutputPercent();
 		// prepare line to print
@@ -113,36 +114,29 @@ public class Robot extends TimedRobot {
 			_sb.append("\terr:");
 			_sb.append(_talon.getClosedLoopError(Constants2.kPIDLoopIdx));
 			_sb.append("\ttrg:");
-			stopBool = true;
 		}
 		else if(_joy.getRawAxis(1) > 0.2 && _talon.getSelectedSensorPosition(0) > 0)
 		{
-			_talon.set(ControlMode.MotionMagic, 0);
+			_talon.set(ControlMode.MotionMagic, 50);
 			_sb.append("\terr:");
 			_sb.append(_talon.getClosedLoopError(Constants2.kPIDLoopIdx));
 			_sb.append("\ttrg:");
-			stopBool = true;
 		}
 		else
 		{
-			if(stopBool) {
-				returnPos = _talon.getSelectedSensorPosition(0);
-				if(_talon.getSelectedSensorVelocity(0) > 0) {
-					returnPos += (_talon.getSelectedSensorVelocity(0) /1.5);
-				} else if(_talon.getSelectedSensorVelocity(0) < 0) {
-					returnPos -= (_talon.getSelectedSensorVelocity(0) * 3.0);
-				}
-				stopBool = false;
+			if(Math.abs(_joy.getRawAxis(1)) > 0.15){
+				returnPos += (_joy.getRawAxis(1) * -400);
+			}
+			
+			// Keep target position within a select range
+			if(returnPos > softLimF) {
+				returnPos = softLimF;
+			} else if (returnPos < softLimR) {
+				returnPos = softLimR;
 			}
 			
 			_talon.set(ControlMode.MotionMagic, returnPos);
 			
-			//if(_joy.getRawAxis(5) > 0.2 || _joy.getRawAxis(5) < 0.2)
-			//{
-			//	_talon.set(ControlMode.PercentOutput, (_joy.getRawAxis(5) * -0.5));
-			//} else {
-			//	_talon.set(ControlMode.PercentOutput, 0);
-			//}
 		}
 		// instrumentation
 		Instrum1.Process(_talon, _sb);
@@ -150,56 +144,7 @@ public class Robot extends TimedRobot {
 			TimeUnit.MILLISECONDS.sleep(10);
 		} catch (Exception e) {
 		}
-		*/
-		if (_joy.getRawButton(6) && (!aPressed)) {
-			aPressed = true;
-			if(count < 4 && count >= 0){
-				count++;
-				_joy.setRumble(GenericHID.RumbleType.kLeftRumble , 0.5);
-				_joy.setRumble(GenericHID.RumbleType.kRightRumble , 0.5);
-				Timer.delay(0.05 + (0.05 * count));
-				_joy.setRumble(GenericHID.RumbleType.kLeftRumble , 0.0);
-				_joy.setRumble(GenericHID.RumbleType.kRightRumble , 0.0);
-			}
-		} else if ((!_joy.getRawButton(6)) && aPressed) {
-			aPressed = false;
-		}
-
-		if (_joy.getRawButton(5) && (!bPressed)) {
-			bPressed = true;
-			if(count <= 4 && count > 0){
-				count--;
-				_joy.setRumble(GenericHID.RumbleType.kLeftRumble , 0.5);
-				_joy.setRumble(GenericHID.RumbleType.kRightRumble , 0.5);
-				Timer.delay(0.05 + (0.05 * count));
-				_joy.setRumble(GenericHID.RumbleType.kLeftRumble , 0.0);
-				_joy.setRumble(GenericHID.RumbleType.kRightRumble , 0.0);
-			}
-		} else if ((!_joy.getRawButton(5)) && bPressed) {
-			bPressed = false;
-		}
-
-		switch(count){
-		case 0:
-			_talon.set(ControlMode.MotionMagic, 0);
-			break;
-		case 1:
-			_talon.set(ControlMode.MotionMagic, 1000);
-			break;
-		case 2:
-			_talon.set(ControlMode.MotionMagic, 2000);
-			break;
-		case 3:
-			_talon.set(ControlMode.MotionMagic, 4000);
-			break;
-		case 4:
-			_talon.set(ControlMode.MotionMagic, 6500);
-			break;
-		default:
-			_talon.set(ControlMode.MotionMagic, 0);
-		}
 	}
-	
 	@Override
 	public void testPeriodic() {
 	}
