5d4c9e2ac65a678bab7be4aff6a7e8468156484f
MasterMemeMinister <soupcateatternom@gmail.com>
2018-02-17 19:09:26 -0600

    Got LEDs to work on robot! First year w/ LEDs!

--- 2018RobotCode/src/org/usfirst/frc/team930/robot/LEDControl/LEDControl.ino
+++ 2018RobotCode/src/org/usfirst/frc/team930/robot/LEDControl/LEDControl.ino
@@ -1,15 +1,15 @@
 //Team 930!!
 
-/* State         Pattern        ID       Strip
- *  Disabled      Red Solid      1        topStrip 
- *  Enabled       Tail blue      2        topStrip
- *  In take       Blink green    3        topStrip
- *  Elevator      Height based   4        sideStrips
- *  Out take      Yellow Solid   5        topStrip
- *  Test Mode     Rainbow        6        topStrip
- *  Ramp down     Tail red       7        topStrip and sideStrip
- *  Ramp up       Tail green     8        topStrip and sideStrip
- *  Autonomous    Blue Solie     9        topStrip
+/* State         Pattern        ID       topStrip
+ *  Disabled      Red Solid      1        toptopStrip 
+ *  Enabled       Tail blue      2        toptopStrip
+ *  In take       Blink green    3        toptopStrip
+ *  Elevator      Height based   4        sidetopStrips
+ *  Out take      Yellow Solid   5        toptopStrip
+ *  Test Mode     Rainbow        6        toptopStrip
+ *  Ramp down     Tail red       7        toptopStrip and sidetopStrip
+ *  Ramp up       Tail green     8        toptopStrip and sidetopStrip
+ *  Autonomous    Blue Solie     9        toptopStrip
  * 
  * "I regret not checking Slack..." -Mark Menning
  */ 
@@ -62,6 +62,12 @@ void loop() {
     disabledPattern();
   } else if (patternID == 2) {
     enabledPattern();
+  } else if (patternID == 3) {
+    inTakePattern();
+  } else if (patternID == 5) {
+    outTakePattern();
+  } else if (patternID == 6) {
+    testPattern();
   }
 }
 
@@ -70,33 +76,40 @@ void loop() {
 //-- Function receiveEvent --\\
 
 void receiveEvent(int howMany) {
-  while (Wire.available()) { // loop through all but the last
-    patternID = Wire.read();     // receive byte as an integer. Sets patternID to the data received. 
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
   }
+  int x = Wire.read();    // receive byte as an integer
+  patternID = x;
 }
 
-//-- Pattern for Disabled --\\
+//-- Pattern for Red Solide (Mark Menning)--\\
 
-void disabledPattern() {
+void testPattern() {
+
   topStrip.setBrightness(BRIGHTNESS);
   for (int i = 0; i < TOP_STRIP_NUMPIXELS; i++) { 
     topStrip.setPixelColor(i, 0, 255, 0 );
   }
   topStrip.show();
-  delay(1);
+  //delay(1);
 }
 
-//-- Pattern for Enabled --\\
+//-- Pattern for Blue Tail (Dominick E.)--\\
 
 void enabledPattern() {
   topStrip.setBrightness(BRIGHTNESS);
   
   topStrip.setPixelColor(enabledPatternHead, 0, 0, 255); // 'On' pixel at head
   topStrip.setPixelColor(enabledPatternTail, 0);     // 'Off' pixel at tail
-  topStrip.show();                                   // Refresh strip
-  delay(10);                                         // Pause 10 milliseconds (~25 FPS)
 
-  if (++enabledPatternHead >= TOP_STRIP_NUMPIXELS) {         // Increment head index.  Off end of strip?
+  topStrip.setPixelColor(enabledPatternHead + 20, 0, 0, 255); // 'On' pixel at head
+  topStrip.setPixelColor(enabledPatternTail + 20, 0);     // 'Off' pixel at tail
+  
+  topStrip.show();                                   // Refresh topStrip
+  delay(1);                                         // Pause 10 milliseconds (~25 FPS)
+
+  if (++enabledPatternHead >= TOP_STRIP_NUMPIXELS) {         // Increment head index.  Off end of topStrip?
     enabledPatternHead = 0;                                 //  Yes, reset head index to start
   }
   
@@ -105,4 +118,57 @@ void enabledPattern() {
   }
 }
 
+//-- Pattern for Rainbow (Trenton B.) --\\
+
+void disabledPattern() {
+  topStrip.setBrightness(BRIGHTNESS);
+  for (int b = 0; b < 6; b++) {
+    for (int i = 0; i < TOP_STRIP_NUMPIXELS; i++) { 
+      if (i % 6 == 0) {
+        topStrip.setPixelColor(i+b, 0, 255, 0 );
+      } else if (i % 5 == 0) {
+        topStrip.setPixelColor(i+b, 128, 255, 0 );
+      } else if (i % 4 == 0) {
+        topStrip.setPixelColor(i+b, 255, 255, 0 );
+      } else if (i % 3 == 0) {
+        topStrip.setPixelColor(i+b, 255, 0, 0 );
+      } else if (i % 2 == 0) {
+        topStrip.setPixelColor(i+b, 0, 0, 255);
+      } else if (i % 1 == 0) {
+        topStrip.setPixelColor(i+b, 0, 128, 255);
+      }
+    }
+    topStrip.show();
+    delay(50); 
+  }
+}
+
+//-- Pattern for Blinking Green (Dominick E.) --\\
+
+void inTakePattern() {
+  topStrip.setBrightness(BRIGHTNESS);
+  
+  for (int i = 0; i < TOP_STRIP_NUMPIXELS; i++) { 
+    topStrip.setPixelColor(i,255,0,0);
+  }
+  topStrip.show();
+  delay(100);
+  
+  for (int i = 0; i < TOP_STRIP_NUMPIXELS; i++) { 
+    topStrip.setPixelColor(i,0,0,0);
+  }
+  topStrip.show();
+  delay(100);
+}
+
+//-- Pattern for Solid Yellow (Mark Menning) --\\
+
+void outTakePattern() {
+  topStrip.setBrightness(BRIGHTNESS);
+  
+  for (int i = 0; i < TOP_STRIP_NUMPIXELS; i++) { 
+    topStrip.setPixelColor(i, 255, 255, 0);
+  }
+  topStrip.show();
+}
 
